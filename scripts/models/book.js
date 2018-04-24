6fab7513fd70ed09eac5909fd4effdbf7e361381
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-04-24 14:42:05 -0700

    changed variable property names to be more semantic, changed development api url

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,6 +1,6 @@
 const ENV = {};
-ENV.prop1 = (window.location.protocol === 'https');
+ENV.isProduction = window.location.protocol === 'https';
 console.log(window.location.protocol);
-ENV.prop2 = 'https://ec-mp-booklist.herokuapp.com';
-ENV.prop3 = '172.16.1.18:3000';
-ENV.prop4 = ENV.prop1 ? ENV.prop2 : ENV.prop3;
+ENV.productionApiUrl = 'https://ec-mp-booklist.herokuapp.com';
+ENV.developmentApiUrl = 'localhost:3000';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
